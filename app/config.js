d3cae5d6aa76e9d5d5f97d1e2bec02b5f1da5c2c
Luis Fedrizze <luisfedrizze@gmail.com>
2020-07-15 00:10:12 -0300

    mongo configuration

--- app/config.js
+++ app/config.js
@@ -1,5 +1,11 @@
 export default {
   token: process.env.DISCORD_BOT_TOKEN,
   debug: (process.env.DEBUG === 'true' ?? false),
-  commandPrefix: '!'
+  commandPrefix: '!',
+  database: {
+    username: process.env.MONGO_USERNAME,
+    password: process.env.MONGO_PASSWORD,
+    host: process.env.MONGO_HOST,
+    port: process.env.MONGO_PORT
+  }
 }
