ab9ddc62f0fc9da44da62b0c7f22ce80a8915e10
Luis Fedrizze <luisfedrizze@gmail.com>
2020-07-19 06:05:05 -0300

    clanadmin validation

--- app/contexts/ClanAdministration.js
+++ app/contexts/ClanAdministration.js
@@ -170,7 +170,10 @@ module.exports = class ClanAdministration extends Context {
       const clanExists = await this.clanRepository.findByRole(clanRole.id)
       if (clanExists) {
         if (!lead.roles.cache.has(clanRole.id)) throw new NotClanLead(clanExists.name)
-        if (member.roles.has(config.roles.clanlead)) throw new CantRemoveLead()
+        if (
+          member.roles.has(config.roles.clanlead) &&
+          !lead.roles.has(config.roles.clanadmin)
+        ) throw new CantRemoveLead()
         const hasRole = member.roles.cache.has(clanRole.id)
         if (hasRole) {
           await member.roles.remove(clanRole.id)
