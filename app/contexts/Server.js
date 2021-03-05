3b634505f0cbcdbad9edb54eec17e8b91c0f3be1
Luis Fedrizze <luisfedrizze@gmail.com>
2021-03-05 05:28:45 -0300

    refactor(gather): move server commands to server context
    mova server commands to server context and

--- app/contexts/Server.js
+++ app/contexts/Server.js
@@ -1,14 +1,12 @@
 const Context = require('../classes/Context')
 const Command = require('../classes/Command')
-const Logger = require('../classes/Logger')
+// const Logger = require('../classes/Logger')
 const GatherServers = require('../repositories/GatherServers')
 const GatherSessions = require('../repositories/GatherSessions')
 const ServerTokens = require('../repositories/ServerTokens')
-const { OnPresence } = require('../classes/Events')
 const Users = require('../repositories/Users')
-const { MD, Channel } = require('../classes/Responses')
 const config = require('../config')
-const log = new Logger('Gather')
+// const log = new Logger('Server')
 
 class Gather extends Context {
   constructor (user, channel, message) {
@@ -16,23 +14,15 @@ class Gather extends Context {
 
     // Commands
     this.commands = [
-      // new Command('breathe', ['server'], this._breathe.bind(this)),
-      new Command('genservertoken', ['gatheradmin'], this._genServerToken.bind(this)),
-      new Command('addctf', ['everyone'], this._add.bind(this)),
-      new Command('addtm', ['everyone'], this._add.bind(this)),
-      new Command('del', ['everyone'], this._remove.bind(this)),
-      // new Command('round', ['server'], this._round.bind(this)),
-      // new Command('playerauth', ['server'], this._playerauth.bind(this)),
-      // new Command('checkplayerauth', ['server'], this._checkPlayerAuth.bind(this)),
-      // new Command('serverready', ['server'], this._serverReady.bind(this)),
-      new Command('info', ['everyone'], this._info.bind(this)),
-      // new Command('tiebreakmap', ['server'], this._tiebreakmap.bind(this)),
-      new Command('spec', ['everyone'], this._spec.bind(this)),
-      // new Command('createsub', ['server'], this._createsub.bind(this)),
-      new Command('sub', ['everyone'], this._addsub.bind(this)),
-      new Command('delsub', ['everyone'], this._delsub.bind(this)),
-      // new Command('callsub', ['server'], this._callsub.bind(this)),
-      // new Command('suboff', ['server'], this._suboff.bind(this))
+      new Command('breathe', ['server'], this._breathe.bind(this)),
+      new Command('round', ['server'], this._round.bind(this)),
+      new Command('playerauth', ['server'], this._playerauth.bind(this)),
+      new Command('checkplayerauth', ['server'], this._checkPlayerAuth.bind(this)),
+      new Command('serverready', ['server'], this._serverReady.bind(this)),
+      new Command('tiebreakmap', ['server'], this._tiebreakmap.bind(this)),
+      new Command('createsub', ['server'], this._createsub.bind(this)),
+      new Command('callsub', ['server'], this._callsub.bind(this)),
+      new Command('suboff', ['server'], this._suboff.bind(this))
     ]
 
     // Vars
@@ -45,29 +35,12 @@ class Gather extends Context {
 
   async validate (roles) {
     const command = this._validateCommands()
-    log.d('Validated command', command)
 
     if (command) {
       const normalizedRoles = this._normalizeRoles(roles)
-      log.d('normalizedRoles', normalizedRoles)
       if (this._validateCommandRoles(command, normalizedRoles)) {
         if (this.channel === config.channels.gather) return command
       }
-    }
-  }
-
-  async _addServer (type) {
-    let [, ip, port, ...name] = this.params
-    name = name.join(' ')
-    const result = await this.gatherRepository.create(ip, port, name, type, 'waiting')
-    return result.ops[0]
-  }
-
-  async _removeServer () {
-    if (this.params.length === 3) {
-      const [, ip, port] = this.params
-      await this.gatherRepository.delete(ip, port)
-      return `Servidor ${ip}:${port} removido com sucesso`
     }
   }
 
@@ -91,78 +64,6 @@ class Gather extends Context {
       }
       return session.state
     }
-  }
-
-  async _add () {
-    const [command] = this.params
-    const type = command === '!addctf' ? 'ctf' : 'tm'
-    const availableSessions = await this.gatherRepository.available(type)
-    if (availableSessions.length > 0) {
-      const session = availableSessions[0]
-      const isPlayerInSession = await this.gatherRepository.findPlayerSession(this.user)
-      if (isPlayerInSession) return 'Você já está em uma fila.'
-      session.players.push(this.user)
-      await this.gatherRepository.addPlayer(session.ip, session.port, this.user)
-
-      if (session.players.length === config.game.maxplayers) {
-        // TODO start the game
-        const sessionId = await this.gatherRepository.startGame(session.ip, session.port)
-        const players = session.players.slice()
-        this._shuffleTeams(players)
-        const alpha = players.slice(0, config.game.alpha)
-        const bravo = players.slice(-1 * config.game.bravo)
-        const tiebreakMap = this._randomATiebreakMap(session.type)
-        await this.gatherSessionsRepository.create(
-          sessionId, alpha, bravo, tiebreakMap
-        )
-        return new Channel(
-          'O jogo já está pronto, estamos preparando o servidor. ' +
-          'Um invite para jogar será enviado via mensagem direta.\n' +
-          'Time Alpha: [' +
-          (
-            alpha.map(memberId => `<@${memberId}>`).join(', ')
-          ) + ']\n' +
-          'Time Bravo: [' +
-          (
-            bravo.map(memberId => `<@${memberId}>`).join(', ')
-          ) + ']\n' +
-          `Mapa de desempate: ${tiebreakMap}`,
-          config.channels.gather
-        )
-      } else {
-        return new Channel(
-          `[${session.type.toUpperCase()}] ${session.name} [` +
-          (
-            session.players.map(memberId => this._memberDisplayName(memberId)).join(', ')
-          ) + ']' + ` (${session.players.length}/${config.game.maxplayers})`,
-          config.channels.gather
-        )
-      }
-    } else {
-      return 'Não há nenhum servidor com vaga no momento. Digite !info para obter a lista de servidores'
-    }
-  }
-
-  async _remove () {
-    let session = await this.gatherRepository.findPlayerSession(this.user)
-    if (session && session.state === 'waiting') {
-      await this.gatherRepository.removePlayer(session.ip, session.port, this.user)
-      session = await this.gatherRepository.find(session.ip, session.port)
-      return new Channel(
-        `[${session.type.toUpperCase()}] ${session.name} [` +
-        (
-          session.players.map(memberId => this._memberDisplayName(memberId)).join(', ')
-        ) + ']' + ` (${session.players.length}/${config.game.maxplayers})`,
-        config.channels.gather
-      )
-    } else {
-      return 'Você não está em uma fila de espera.'
-    }
-  }
-
-  async _genServerToken () {
-    const token = await this.serverTokensRepository.generate(config.roles.server)
-    return new MD(token)
   }
 
   async _playerauth () {
@@ -292,25 +193,6 @@ class Gather extends Context {
     }
   }
 
-  async _info (event) {
-    const servers = await this.gatherRepository.allNotOffline()
-    if (servers.length > 0) {
-      const serversSummary = servers.map(server => {
-        return `${server.name}\n` +
-        `Status: ${server.state}\n` +
-        `Game style: ${server.type.toUpperCase()}\n` +
-        `Jogadores (${server.players.length}/${config.game.maxplayers}): [` +
-        (
-          server.players.map(player => {
-            const member = event.guild.members.cache.get(player)
-            return member ? member.displayName : '?'
-          }).join(', ')
-        ) + ']\n\n'
-      }).join('')
-      event.channel.send('-\n' + serversSummary)
-    }
-  }
-
   async _tiebreakmap () {
     const [, ip, port] = this.params
     const server = await this.gatherRepository.find(ip, port)
@@ -318,17 +200,6 @@ class Gather extends Context {
       const session = await this.gatherSessionsRepository.find(server.sessionId)
       if (session) {
         return session.maps.tie.mapName
-      }
-    }
-  }
-
-  async _spec (event) {
-    const [, serverNumber] = this.params
-    if (serverNumber) {
-      const servers = await this.gatherRepository.getByNumber(serverNumber)
-      if (servers.length > 0) {
-        const server = servers[0]
-        event.member.send(`soldat://${server.ip}:${server.port}/${server.password}`)
       }
     }
   }
@@ -343,40 +214,6 @@ class Gather extends Context {
       return '1'
     }
     return '0'
-  }
-
-  async _addsub () {
-    const playerAlreadyInQueue = await this.gatherRepository.findPlayerInSubQueue(this.user)
-    if (playerAlreadyInQueue) return 'Você já está em uma fila de sub. Digite !delsub para sair dela.'
-    const servers = await this.gatherRepository.getByOpenedSubSlots()
-    if (servers.length > 0) {
-      const server = servers[0]
-      server.subs.push(this.user)
-      await this.gatherRepository.addSub(server.ip, server.port, this.user)
-      return new Channel(
-        `[${server.type.toUpperCase()}-SUB] ${server.name} [` +
-        (
-          server.subs.map(memberId => this._memberDisplayName(memberId)).join(', ')
-        ) + ']' + ` (${server.subs.length}/${server.subslots})`,
-        config.channels.gather
-      )
-    } else {
-      return 'Não há filas abertas.'
-    }
-  }
-
-  async _delsub () {
-    let server = await this.gatherRepository.findPlayerInSubQueue(this.user)
-    if (!server) return 'você não está em uma fila de sub.'
-    await this.gatherRepository.removeSub(server.ip, server.port, this.user)
-    server = await this.gatherRepository.find(server.ip, server.port)
-    return new Channel(
-      `[${server.type.toUpperCase()}-SUB] ${server.name} [` +
-      (
-        server.subs.map(memberId => this._memberDisplayName(memberId)).join(', ')
-      ) + ']' + ` (${server.subs.length}/${server.subslots})`,
-      config.channels.gather
-    )
   }
 
   async _callsub () {
@@ -400,31 +237,16 @@ class Gather extends Context {
     return '1'
   }
 
-  // Events methods
-  static async _removeOfflinePlayersFromQueue (event) {
-    const gatherRepository = new GatherServers()
-    if (event.user.presence.status === 'offline') {
-      const server = await gatherRepository.findPlayerSession(event.member.id)
-      if (server && server.state === 'waiting') {
-        await gatherRepository.removePlayer(server.ip, server.port, event.member.id)
-        log.i(`Player ${event.member.id} removed from server queue ${server.name}`)
-      }
-    }
+  // Help methods
+  async _addServer (type) {
+    let [, ip, port, ...name] = this.params
+    name = name.join(' ')
+    const result = await this.gatherRepository.create(ip, port, name, type, 'waiting')
+    return result.ops[0]
   }
 
-  // Help methods
   _generatePin (plus) {
     return plus + Math.floor(Math.random() * 99)
-  }
-
-  _shuffleTeams (players) {
-    for (let i = players.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1))
-      const x = players[i]
-      players[i] = players[j]
-      players[j] = x
-    }
-    return players
   }
 
   _compondScores (session) {
@@ -458,19 +280,6 @@ class Gather extends Context {
   _gatherChannel () {
     return this._guild().channels.cache.get(config.channels.gather)
   }
-
-  _memberDisplayName (id) {
-    const guild = this._guild()
-    if (guild) {
-      const member = guild.members.cache.get(id)
-      if (member) return member.displayName
-    }
-
-    return '?'
-  }
 }
-
-// Events binds
-OnPresence.subscribe(Gather._removeOfflinePlayersFromQueue.bind(this))
 
 module.exports = Gather
